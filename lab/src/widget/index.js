b4e41e63cf29a43823167374d393ec9e24b5b115
malinkacat <gumnickajaliza@gmail.com>
2022-12-07 14:34:26 +0200

    update

--- lab/src/widget/index.js
+++ lab/src/widget/index.js
@@ -5,18 +5,13 @@ import axios from 'axios';
 import icon from "./imgs/geo-alt-fill.svg"
 
 function Widget (props) {
-    const [isLoad, setIsLoad] = useState(false);
     const [Weatherdata, setWeatherdata] = useState(weatherMock);
     useEffect(() => {
-        setIsLoad(false);
         axios
         .get(`https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=3b704d72defefe06c75074c745ba6b87`)
         .then (response => {
-          console.log(response.data);
           setWeatherdata(response.data);
-          setIsLoad(true);
         }).catch(err => {
-          console.log(err);
           switch (err.response.status) {
             case 401: {
               alert("Wrong api key was used");
@@ -39,9 +34,7 @@ function Widget (props) {
 
     return(
         <div className="widget">
-
           <div className="greeting-container">
-
             <div id="location-tag">
               <div><img src={icon} /></div>
               <div>
@@ -49,16 +42,11 @@ function Widget (props) {
                 <p id="city-name">{props.city}</p>
               </div>
             </div>
-
             <div className="greeting-title">Weather Widget</div>
             <div className="line"></div>
-
           </div>
-
             <div className="info-container">
-
               <div className="info-title">General Info</div>
-
               <div className="general-info-container">
                 <div>
                 <div id="temperature">{(Weatherdata.main.temp - 273.15).toFixed(0)}°C</div>
@@ -67,9 +55,7 @@ function Widget (props) {
                   {Weatherdata.weather.map(el => (<div><img src={`http://openweathermap.org/img/w/${el.icon}.png`} alt='' /></div>))}
                 </div>
                 </div>
-
                 <div id="vertical-hr"></div>
-
                 <div id="info">
                   <div>Feels Like: {(Weatherdata.main.feels_like - 273.15).toFixed(0)}°C</div>
                   <div>Humidity: {Weatherdata.main.humidity}%</div>
@@ -77,11 +63,8 @@ function Widget (props) {
                   <div>Longitude: {Weatherdata.coord.lon}</div>
                   <div>Latitude: {Weatherdata.coord.lat}</div>
                 </div>
-                
               </div>
-
               <div className="info-title">More</div>
-
               <div className="more-info-container">
                 <div id="subcontainer">
                   <div className="info-subtitle">Wind</div>
@@ -89,14 +72,12 @@ function Widget (props) {
                   <div id="info-text">Direction: {Weatherdata.wind.deg}°</div>
                   <div id="info-text">Gust: {Weatherdata.wind.gust}m/s</div>
                 </div>
-
                 <div id="subcontainer">
                   <div className="info-subtitle">Other</div>
                   <div id="info-text">Cloudiness: {Weatherdata.clouds.all}%</div>
                   <div id="info-text">Visibility: {Weatherdata.visibility}m</div>
                 </div>
               </div>
-            
         </div>
       </div>
     );
